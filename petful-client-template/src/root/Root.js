281691b808ff08f00e8496908f0bf5d4ee101322
tonyg <aj.gianno@gmail.com>
2021-02-25 17:51:33 -0600

    queue

--- petful-client-template/src/root/Root.js
+++ petful-client-template/src/root/Root.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function Root() {
-  return <div>
-    <h1>Petful</h1>
-  </div>
+  return (
+    <div>
+      <h1>Petful</h1>
+    </div>
+  );
 }
 
-export default Root
+export default Root;
