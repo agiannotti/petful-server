187f2a2f2e7024948dec5a3a169e20d7bfd2b004
tonyg <aj.gianno@gmail.com>
2021-02-25 18:48:00 -0600

    routers n routers

--- petful-server-template/modules/app/app.js
+++ petful-server-template/modules/app/app.js
@@ -8,5 +8,6 @@ app.use(cors());
 app.use('/people', require('../people/people.router'));
 app.use('/pets', require('../pets/pets.router'));
 app.use('/cats', require('../cats/cats.router'));
+app.use('/dogs', require('../dogs/dogs.router'));
 
 module.exports = app;
--- petful-server-template/modules/dogs/dogs.router.js
+++ petful-server-template/modules/dogs/dogs.router.js
@@ -6,7 +6,7 @@ const Pets = require('../pets/pets.service');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const cat = Pets.getCat();
+  const cat = Pets.getDog();
   return res.status(200).json(cat);
 });
 
